319fb9f50feb09ddb9ba49a9dcb41619bd670f96
pato <patrickpato368@gmail.com>
2025-08-09 18:31:54 +0300

    Add mobile as a regular folder

---
+++ mobile/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import '../global.css'; // Import global styles
+
+export default function Index() {
+  return (
+    <View
+     className="flex-1 items-center justify-center bg-white" >
+      <Text  className="text-2xl font-bold text-center" >Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
